843bd27f29cd2e3bd781577a938e8bd0af9f5be8
Christidis Aristarchos <aris.kapios@yahoo.gr>
2021-12-19 12:10:36 +0200

    extra Status Controller

--- Game.php
+++ Game.php
@@ -6,10 +6,39 @@
 		<link rel = "stylesheet" href = "styles/gameStyle.css">
 	</head>
 	<script>
-		function checkGames() {
-			
-		}
 	
+		const xmlHttp = new XMLHttpRequest();
+		var PlayerName;
+		function checkGames() {
+			PlayerName = document.getElementById("player").innerHTML;
+			xmlHttp.open("GET", "\gameControll.php?username=" + PlayerName, true);
+			xmlHttp.onreadystatechange = respond;
+			xmlHttp.send();
+		}
+		function respond() {
+			if (xmlHttp.readyState == 4) {
+				if (xmlHttp.status == 200) {
+					var response = xmlHttp.responseText;
+					var res =  document.getElementById("Player1DIV");
+					res.innerHTML = "Server responded with text = " + response;
+					
+					if (response.substr(0,7) == "created")
+						document.getElementById("name1").innerHTML = PlayerName;
+					else if (response != PlayerName) {
+						document.getElementById("name1").innerHTML = response;
+						document.getElementById("name2").innerHTML = PlayerName;
+					}
+				}
+				else
+					alert("An error has occured with the response");
+			}
+		}
+		
+		var interval = setInterval(checkStatus,1000);
+		function checkStatus() {
+			
+		}			
+		
 	</script>
 	
 	<body>
@@ -18,9 +47,14 @@
 				<button type = "button" onclick = "checkGames()"> I am ready </button>
 				<br>
 				Info : 
+				<span id = 'player'>
+					<?php 
+						session_start();
+						echo $_SESSION['usern']; 
+					?>
+				</span>
 				<?php
-					session_start();
-					echo "<br>player 1 :".$_SESSION['usern'];
+					echo "<br>player 1 :<span id = 'name1'></span> player 2:<span id = 'name2'> </span>";
 				?>
 			</div>
 			<div id = "gamePanel">
--- gameControll.php
+++ gameControll.php
@@ -1,25 +1,36 @@
 <?php
+	$user = $_GET["username"];
 	$HOST = "localhost";
 	$username = "root";
 	$password = "";
 	$DATABASE = "gamedb";
 	$conn = new mysqli($HOST,$username,$password,$DATABASE);
 	
-	$sql = "select * from game where GameStatus = 'waiting'";
+	$sql = "select * from game where user1 = '".$user."' or user2 = '".$user."'";
 	$result = $conn->query($sql);
+	if ($result->num_rows == 0) { 
 	
-	if ($result->num_rows > 0) {
-		$row = $result->fetch_assoc();
-		$gid = $row["Gid"];
-		echo "yparxoun painxidia";
+		$sql = "select * from game where GameStatus = 'waiting'";
+		$result = $conn->query($sql);
+		
+		if ($result->num_rows > 0) {
+			$row = $result->fetch_assoc();
+			$gid = $row["Gid"];
+			$user1 = $row["user1"];
+			$sql = "update game set GameStatus = 'started' , user2 = '".$user."' where Gid = '".$gid."'";
+			if ($conn->query($sql) === TRUE) {
+				echo $user1;
+			} else {
+				echo "<br>Error: " . $sql . "<br>" . $conn->error;
+			}
+		}
+		else {
+			//den yparxoun paixnidia ara prepei na ftiaksw ena.
+			echo "created";
+			$sql = "insert into game(user1,GameStatus,turn) values('".$user."','waiting',1)";
+			$conn->query($sql);
+		}
 	}
-	else {
-		//den yparxoun paixnidia ara prepei na ftiaksw ena.
-		echo "den yparxoun paixnidia me anamoni";
-	}
-	
-	
-	
 	
 	$conn->close();
 ?>
